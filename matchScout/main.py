a349eec6836517c813208f258335f6a6e4f19870
hledbetter124 <79533649+hledbetter124@users.noreply.github.com>
2022-03-30 22:20:51 -0500

    added match number

--- matchScout/main.py
+++ matchScout/main.py
@@ -13,14 +13,18 @@ def goodExcel():
     goodSheet.column_dimensions['B'].width = 10
     goodSheet.column_dimensions['C'].width = 10
     goodSheet.column_dimensions['D'].width = 10
-    goodSheet.column_dimensions['E'].width = 20
+    goodSheet.column_dimensions['E'].width = 10
     goodSheet.column_dimensions['F'].width = 20
+    goodSheet.column_dimensions['G'].width = 20
+    goodSheet.column_dimensions['H'].width = 10
     goodSheet.cell(row=1, column=1).value = "Team #"
-    goodSheet.cell(row=1, column=2).value = "ballsScoredLower"
-    goodSheet.cell(row=1, column=3).value = "ballsScoredUpper"
-    goodSheet.cell(row=1, column=4).value = "Highest Climb Level"
-    goodSheet.cell(row= 1, column=7).value = "Autonomous y/n"
-    goodSheet.cell(row=1, column=9).value = "Stability (1-10) 1 = getting flipped"
+    goodSheet.cell(row=1, column=2).value = "Match #"
+    goodSheet.cell(row=1, column=3).value = "ballsScoredLower"
+    goodSheet.cell(row=1, column=4).value = "ballsScoredUpper"
+    goodSheet.cell(row=1, column=5).value = "Highest Climb Level"
+    goodSheet.cell(row=1, column=6).value = "Autonomous y/n"
+    goodSheet.cell(row=1, column=7).value = "Stability (1-10) 1 = getting flipped"
+    goodSheet.cell(row=1, column=8).value = "Fouls"
 def focus1(event):
     ballsScoredLower.focus_set()
 def focus2(event):
@@ -32,7 +36,8 @@ def focus4(event):
 def focus5(event):
     stability.focus_set()
 def focus6(event):
-    badOrGood.focus_set()
+    foulPoints.focus_set()
+
 def clear():
     teamNum.delete(0, END)
     ballsScoredLower.delete(0, END)
@@ -53,11 +58,13 @@ def sheet():
         current_row = goodSheet.max_row
         current_column = goodSheet.max_column
         goodSheet.cell(row=current_row + 1, column=1).value = teamNum.get()
-        goodSheet.cell(row=current_row + 1, column=2).value = lowerCount
-        goodSheet.cell(row=current_row + 1, column=3).value = upperCount
-        goodSheet.cell(row=current_row + 1, column=4).value = climbLevel.get()
-        goodSheet.cell(row=current_row + 1, column=5).value = auto.get()
-        goodSheet.cell(row=current_row + 1, column=6).value = stability.get()
+        goodSheet.cell(row=current_row + 1, column=2).value = matchNum.get()
+        goodSheet.cell(row=current_row + 1, column=3).value = lowerCount
+        goodSheet.cell(row=current_row + 1, column=4).value = upperCount
+        goodSheet.cell(row=current_row + 1, column=5).value = climbLevel.get()
+        goodSheet.cell(row=current_row + 1, column=6).value = auto.get()
+        goodSheet.cell(row=current_row + 1, column=7).value = stability.get()
+        goodSheet.cell(row=current_row + 1, column=8).value = foulPoints.get()
         goodwb.save('teams.xlsx')
         teamNum.focus_set()
         clear()
@@ -81,25 +88,31 @@ if __name__ == "__main__":
     root.configure(background='blue')
     root.title("FRC Scouting")
     # set the configuration of GUI window
-    root.geometry("400x700")
+    root.geometry("695x800")
     goodExcel()
     heading = Label(root, text="Scouting Form", bg="white")
     num= Label(root, text="Team Number", bg="white")
+    match= Label(root, text="Match Number", bg="White")
     lower = Label(root, text="Balls Scored Lower", bg="White")
     upper = Label(root, text="Balls Scored Upper", bg="White")
     climb = Label(root, text="Climb Level", bg="White")
     stable = Label(root, text="Rate stability 1-10 (1 being they flipped mid match)", bg="White")
     autonomous = Label(root, text="Did they do something during autonomous?", bg="White")
     theDecision = Label(root, text="Save after each match", bg="white")
+    fouls = Label(root, text="How many fouls were called against them (if any)?", bg='White')
     heading.grid(row=0, column=0)
     num.grid(row=1, column=0)
-    lower.grid(row=3, column=0)
-    upper.grid(row=5, column=0)
-    climb.grid(row=7, column=0)
-    autonomous.grid(row=9, column=0)
-    stable.grid(row=11, column=0)
-    theDecision.grid(row=13, column=0)
+    match.grid(row=3, column=0)
+    lower.grid(row=5, column=0)
+    upper.grid(row=7, column=0)
+    climb.grid(row=9, column=0)
+    autonomous.grid(row=11, column=0)
+    stable.grid(row=13, column=0)
+    theDecision.grid(row=17, column=0)
+    fouls.grid(row=15, column=0)
+    foulPoints = Entry(root)
     teamNum = Entry(root)
+    matchNum = Entry(root)
     ballsScoredLower = Entry(root)
     ballsScoredUpper = Entry(root)
     climbLevel = Entry(root)
@@ -107,22 +120,24 @@ if __name__ == "__main__":
     stability = Entry(root)
     badOrGood = Entry(root) 
     teamNum.bind("<Return>", focus1)
-    ballsScoredLower.bind("<Return>", focus2)
-    ballsScoredUpper.bind("<Return>", focus3)
+    ballsScoredLower.bind("<Return>", lowerButtonPress)
+    ballsScoredUpper.bind("<Return>", upperButtonPress)
     climbLevel.bind("<Return>", focus4)
     autonomous.bind("<Return>", focus5)
     stability.bind("<Return>", focus6)
-    teamNum.grid(row=2, column=0, ipadx="25")
+    teamNum.grid(row=2, column=0, ipadx=25)
+    matchNum.grid(row = 4, column=0, padx=25)
     lowerButton = Button(root, text="+1 Lower", fg="White", bg="Black", command=lowerButtonPress)
-    lowerButton.grid(row=4, column=0, ipadx="25")
+    lowerButton.grid(row=6, column=0, ipadx=25)
     upperButton = Button(root, text="+1 Upper", fg="White", bg="Black", command=upperButtonPress)
-    upperButton.grid(row=6, column=0, ipadx="25")
-    climbLevel.grid(row=8, column=0, ipadx="25")
-    auto.grid(row=10, column=0, ipadx="25")
-    stability.grid(row=12, column=0, ipadx="25") 
+    upperButton.grid(row=8, column=0, ipadx=25)
+    climbLevel.grid(row=10, column=0, ipadx=25)
+    auto.grid(row=12, column=0, ipadx=25)
+    stability.grid(row=14, column=0, ipadx=25) 
+    foulPoints.grid(row=16, column=0, ipadx=25)
     goodExcel()
     good = Button(root, text="Save", fg="Black", bg="Green", command=sheet)
-    good.grid(row=15, column=0)
+    good.grid(row=18, column=0)
     upload = Button(root, text="upload to drive", fg="light blue", bg="Black", command=updateFile)
-    upload.grid(row=16, column=0)
+    upload.grid(row=19, column=0)
     root.mainloop()
